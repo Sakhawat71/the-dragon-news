9f2422335f9c6daaf61ab8011ae6fdcd83fcb3c7
sakhawat71 <sakhawat.showrav@gmail.com>
2024-07-26 23:41:58 +0600

    every home items connected with link

--- src/components/ui/LatestNews/LatestNews.js
+++ src/components/ui/LatestNews/LatestNews.js
@@ -3,6 +3,7 @@ import React from 'react';
 import NewsCard from '../NewsCard.js/NewsCard';
 import { getFrontPageNews } from '@/utils/getFrontPageNews';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const LatestNews = async () => {
 
@@ -12,40 +13,44 @@ const LatestNews = async () => {
     return (
         <Grid container spacing={3} >
             <Grid item xs={12}>
-                <Card>
-                    <CardActionArea>
-                        <CardMedia sx={{
-                            '& img': {
-                                height: '450px'
-                            }
-                        }}>
-                            <Image
-                                src={latest?.thumbnail_url}
-                                width={800}
-                                height={450}
-                                alt='latest thambnel'
-                            />
-                        </CardMedia>
+                <Link href={`/${latest.category}/${latest._id}`}>
+                    <Card>
+                        <CardActionArea>
+                            <CardMedia sx={{
+                                '& img': {
+                                    height: '450px'
+                                }
+                            }}>
+                                <Image
+                                    src={latest?.thumbnail_url}
+                                    width={800}
+                                    height={450}
+                                    alt='latest thambnel'
+                                />
+                            </CardMedia>
 
-                        <CardContent>
-                            <Typography className='bg-[#FF8C47] w-fit px-1 rounded-lg text-white'>
-                                {latest?.category}
-                            </Typography>
-                            <Typography gutterBottom variant="h5" component="div">
-                                {latest?.title.length > 50 ? latest?.title.slice(0, 30) + ` ...` : latest?.title}
-                            </Typography>
+                            <CardContent>
+                                <Typography className='bg-[#FF8C47] w-fit px-1 rounded-lg text-white'>
+                                    {latest?.category}
+                                </Typography>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {latest?.title.length > 50 ? latest?.title.slice(0, 30) + ` ...` : latest?.title}
+                                </Typography>
 
-                            <Typography variant="body1" color="text.primay">
-                                Post by {latest?.author.name}
-                            </Typography>
+                                <Typography variant="body1" color="text.primay">
+                                    Post by {latest?.author.name}
+                                </Typography>
 
-                            <Typography variant="body2" color="text.secondary">
-                                {latest?.details.length > 150 ? latest?.details.slice(0, 150) + ` ...` : latest?.details}
-                            </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    {latest?.details.length > 150 ? latest?.details.slice(0, 150) + ` ...` : latest?.details}
+                                </Typography>
 
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Link>
+
+
             </Grid>
 
             {
