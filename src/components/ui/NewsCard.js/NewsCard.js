5a7e622d234fcc25ffe62fecd40966ce9ddf07cc
sakhawat71 <sakhawat.showrav@gmail.com>
2024-07-26 23:39:51 +0600

    use link to home page items

--- src/components/ui/NewsCard.js/NewsCard.js
+++ src/components/ui/NewsCard.js/NewsCard.js
@@ -1,45 +1,48 @@
 
 import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
 import Image from 'next/image';
+import Link from 'next/link';
 
-const NewsCard = ({news}) => {
+const NewsCard = ({ news }) => {
 
 
     return (
-        <Card>
-            <CardActionArea>
-                <CardMedia sx={{
-                    '& img' : {
-                        height: '250px'
-                    } 
-                }}>
-                    <Image
-                        src={news?.thumbnail_url}
-                        width={800}
-                        height={200}
-                        alt='news thambnel'
-                    />
-                </CardMedia>
+        <Link href={`/${news.category}/${news._id}`}>
+            <Card>
+                <CardActionArea>
+                    <CardMedia sx={{
+                        '& img': {
+                            height: '250px'
+                        }
+                    }}>
+                        <Image
+                            src={news?.thumbnail_url}
+                            width={800}
+                            height={200}
+                            alt='news thambnel'
+                        />
+                    </CardMedia>
 
-                <CardContent>
-                    <Typography className='bg-[#FF8C47] w-fit px-1 rounded-lg text-white'>
-                        {news?.category}
-                    </Typography>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {news?.title.length > 50 ? news?.title.slice(0,30) + ` ...` : news?.title}
-                    </Typography>
+                    <CardContent>
+                        <Typography className='bg-[#FF8C47] w-fit px-1 rounded-lg text-white'>
+                            {news?.category}
+                        </Typography>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {news?.title.length > 50 ? news?.title.slice(0, 30) + ` ...` : news?.title}
+                        </Typography>
 
-                    <Typography variant="body1" color="text.primay">
-                        Post by {news?.author.name}
-                    </Typography>
+                        <Typography variant="body1" color="text.primay">
+                            Post by {news?.author.name}
+                        </Typography>
 
-                    <Typography variant="body2" color="text.secondary">
-                        {news?.details.length > 150 ? news?.details.slice(0,150) + ` ...` : news?.details}
-                    </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {news?.details.length > 150 ? news?.details.slice(0, 150) + ` ...` : news?.details}
+                        </Typography>
 
-                </CardContent>
-            </CardActionArea>
-        </Card>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </Link>
     );
 };
 
